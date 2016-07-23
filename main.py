2e83c1ca326c05d3d0f6a4d6ddb65807e26ffee4
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2016-07-23 18:05:36 +0200

    New board printing

--- main.py
+++ main.py
@@ -24,38 +24,77 @@ from random import randint
 
 class Mine(object):
     first = True
+    mine = u'¤'
+    count_mines = 10
+    board_x = 20
+    board_y = 5
     mines = []
     numbers = []
+    _board = ['---------------------------------------',
+              '|xxxxxxxxxxxxxxxxxxx',
+              '|xxxxxxxxxxxxxxxxxxx',
+              '|xxxxxxxxxxxxxxxxxxx',
+              '|xxxxxxxxxxxxxxxxxxx',
+              '|xxxxxxxxxxxxxxxxxxx',
+              '---------------------------------------']
+    board = _board[:]
 
     def __init__(self, **kwargs):
         super(Mine, self).__init__(**kwargs)
         if self.first:
             self.start()
-        self.print_it()
+        self.print_board()
         while True:
             self.check()
 
-    def print_it(self):
-        print '---------------------------------------'
-        print '|x|x|x|x|x|x|x|x|x|x|x|x|x|x|x|x|x|x|x|'
-        print '|x|x|x|x|x|x|x|x|x|x|x|x|x|x|x|x|x|x|x|'
-        print '|x|x|x|x|x|x|x|x|x|x|x|x|x|x|x|x|x|x|x|'
-        print '|x|x|x|x|x|x|x|x|x|x|x|x|x|x|x|x|x|x|x|'
-        print '|x|x|x|x|x|x|x|x|x|x|x|x|x|x|x|x|x|x|x|'
-        print '---------------------------------------'
+    def print_board(self, x=None, y=None, end=False):
+        if not x and not y:
+            for _row in self.board:
+                row = []
+                for r in _row:
+                    if r != '|' and r != '-':
+                        row.append(r+'|')
+                    else:
+                        row.append(r)
+                print ''.join(row)
+        else:
+            if not end:
+                _x = x-1
+                _y = y-1
+                pos = (_x) * _y + _x
+                print 'num length', len(self.numbers)
+                print self.numbers[pos]
+                print self.numbers[pos][2]
+            else:
+                self.board[y][x] = self.mine
+            for _row in self.board:
+                row = []
+                for r in _row:
+                    if r != '|' and r != '-':
+                        row.append(r+'|')
+                    else:
+                        row.append(r)
+                print ''.join(row)
+            if end:
+                print 'Game Over'
+                if raw_input('Play again? y/n') != 'y':
+                    exit()
+                else:
+                    #reset here
+                    pass
 
     def start(self):
-        for i in range(10):
-            x = randint(1, 21)
-            y = randint(1, 6)
+        while len(self.mines) < self.count_mines:
+            x = randint(1, self.board_x+1)
+            y = randint(1, self.board_y+1)
             if [x, y] not in self.mines:
                 self.mines.append([x, y])
         self.first = False
         self.nums()
 
     def nums(self):
-        for i in range(1, 6):
-            for j in range(1, 21):
+        for i in range(1, self.board_y+1):
+            for j in range(1, self.board_x+1):
                 m = 0
                 if [j-1, i] in self.mines:
                     m += 1
@@ -74,13 +113,24 @@ class Mine(object):
                 if [j-1, i+1] in self.mines:
                     m += 1
                 self.numbers.append([j, i, m])
+        _numbers = []
+        for x in xrange(0, len(self.numbers), self.board_x):
+            _numbers.append(self.numbers[x:x+self.board_x])
+        self.numbers = _numbers[:]
 
     def check(self):
         x = input('x >>> ')
         y = input('y >>> ')
+        if x < 0 or y < 0:
+            print 'Only positive numbers!'
+            return
         print self.mines
         print self.numbers
-        print 'Gotcha!' if [x, y] in self.mines else 'Try again!'
+        #print 'Gotcha!' if [x, y] in self.mines else 'Try again!'
+        try:
+            self.print_board(x, y, [x, y] in self.mines)
+        except IndexError:
+            print 'Out of bounds!'
         # show area - for loop, check near mine
         #     if 0 reveal
         #     if 0 in near, reveal too
