2111629fbd88fbe5ef94b2dd39a6376cf018d246
Peter Badida <KeyWeeUsr@users.noreply.github.com>
2017-05-17 13:21:04 +0200

    Fix py3 and game end

--- main.py
+++ main.py
@@ -21,7 +21,12 @@
 
 from __future__ import print_function
 from random import randint
+import sys
 import os
+
+if sys.version_info[0] > 2:
+    raw_input = input
+    xrange = range
 
 
 class Minesweeper(object):
@@ -31,8 +36,7 @@ class Minesweeper(object):
  | | | | |   | -_|_ -| | | | -_| -_| . | -_|  _|
  |_|_|_|_|_|_|___|___|_____|___|___|  _|___|_|
                                    |_|
-                                       by KeyWeeUsr
-'''
+                                       by KeyWeeUsr\n'''
 
     def __init__(self, **kwargs):
         super(Minesweeper, self).__init__(**kwargs)
@@ -40,6 +44,7 @@ class Minesweeper(object):
         # set initial values
         self.mines = []
         self.mine = u'¤'
+        self.cover = 'x'
         self.empty = ' '
         self.spacing = 1
         self.padding = 2
@@ -49,45 +54,57 @@ class Minesweeper(object):
         self.board_x = 20
         self.found_mine = '*'
         self.count_mines = 10
-        self.difficulty = [[9, 9, 10],
-                           [16, 16, 40],
-                           [32, 16, 99]]
+
+        # X, Y, mines count
+        self.difficulty = [
+            [9, 9, 10],
+            [16, 16, 40],
+            [32, 16, 99]
+        ]
+
+        # set padding & spacing
+        self.pad = self.empty * self.padding
+        self.spa = self.empty * self.spacing
 
         # set menu layout
-        self.main_menu = ['(1) Play Game',
-                          '(2) Instructions',
-                          '(3) Options',
-                          '(4) About',
-                          '(5) Exit Game']
+        self.main_menu = [
+            '(1) Play Game',
+            '(2) Instructions',
+            '(3) Options',
+            '(4) About',
+            '(5) Exit Game'
+        ]
 
-        self.menu_values = [['(1) Beginner',
-                             '(2) Intermediate',
-                             '(3) Expert',
-                             '(4) Custom'],
-                            [('1) Select a place in the minefield by typing '
-                              'correct values of X and Y.'),
-                             ('2) Select a mine by typing asterisk(*) after '
-                              'both X and Y coordinates -> e.g. 1* 1*'),
-                             ('3) Exit game either with Ctrl+C or '
-                              'Ctrl+Pause/Break combination.')],
-                            ['Options are set for a single game only:',
-                             '(1) Mine character (default: ' + self.mine + ')',
-                             '(2) Found mine character (default: ' +
-                             self.found_mine + ')',
-                             '(3) Empty place character - zero mines around ' +
-                             '(default: "' + self.empty + '")',
-                             '(4) Padding - from left (default: ' +
-                             str(self.padding) + ')',
-                             '(5) Spacing - between places in minefield ' +
-                             '(default: ' + str(self.spacing) + ')'],
-                            [('This Minesweeper is inspired by the old Win95 '
-                              'game I used to play long ago.'),
-                             ('Even now I play some clone on android once in '
-                              'a while, so...'),
-                             ('I decided to make a text version controlled '
-                              'by a keyboard.'),
-                             (' '*50 + 'Have fun!')],
-                            '']
+        self.menu_values = [
+            ['(1) Beginner',
+             '(2) Intermediate',
+             '(3) Expert',
+             '(4) Custom'],
+            [('1) Select a place in the minefield by typing '
+              'correct values of X and Y.'),
+             ('2) Select a mine by typing asterisk(*) after '
+              'both X and Y coordinates\n' + self.pad +
+              '-> e.g. 1* 1*'),
+             ('3) Exit game either with Ctrl+C or '
+              'Ctrl+Pause/Break combination.')],
+            ['Options are set for a single game only:',
+             '(1) Mine character (default: ' + self.mine + ')',
+             '(2) Found mine character (default: ' +
+             self.found_mine + ')',
+             '(3) Empty place character - zero mines around ' +
+             '(default: "' + self.empty + '")',
+             '(4) Padding - from left (default: ' +
+             str(self.padding) + ')',
+             '(5) Spacing - between places in minefield ' +
+             '(default: ' + str(self.spacing) + ')'],
+            [('This Minesweeper is inspired by the old Win95 '
+              'game I used to play long ago.'),
+             ('Even now I play some clone on android once in '
+              'a while, so...'),
+             ('I decided to make a text version controlled '
+              'by a keyboard.'),
+             (' ' * 50 + 'Have fun!')],
+            '']
 
         self.clean()
         self.menu()
@@ -101,25 +118,25 @@ class Minesweeper(object):
 
     def menu(self):
         for m in self.main_menu:
-            print(' '*self.padding + m)
+            print(self.pad + m)
 
         try:
-            select = int(raw_input('\n' + ' '*self.padding + '>>> '))
+            select = int(raw_input('\n' + self.pad + '>>> '))
             self.clean()
 
             for v in self.menu_values[select - 1]:
-                print(' '*self.padding + v)
+                print(self.pad + v)
 
             if select == 1:
-                value = int(raw_input('\n' + ' '*self.padding + '>>> '))
+                value = int(raw_input('\n' + self.pad + '>>> '))
                 if value != 4:
                     self.board_x = self.difficulty[value - 1][0]
                     self.board_y = self.difficulty[value - 1][1]
                     self.count_mines = self.difficulty[value - 1][2]
                 else:
-                    x = int(raw_input(' '*self.padding + 'width >>> '))
-                    y = int(raw_input(' '*self.padding + 'height >>> '))
-                    m = int(raw_input(' '*self.padding + '# of mines >>> '))
+                    x = int(raw_input(self.pad + 'width >>> '))
+                    y = int(raw_input(self.pad + 'height >>> '))
+                    m = int(raw_input(self.pad + '# of mines >>> '))
                     if x == 1 and y == 1 or m < 1 or x < 1 or y < 1:
                         raise
                     self.board_x = x
@@ -132,8 +149,8 @@ class Minesweeper(object):
                 self.menu()
 
             elif select == 3:
-                option = int(raw_input('\n' + ' '*self.padding + '>>> '))
-                value = raw_input('\n' + ' '*self.padding + '>>> ')
+                option = int(raw_input('\n' + self.pad + '>>> '))
+                value = raw_input('\n' + self.pad + '>>> ')
                 if option == 1:
                     self.mine = value
                 elif option == 2:
@@ -162,12 +179,12 @@ class Minesweeper(object):
             self.menu()
 
     def set_board(self):
-        self._board.append('-' + '-'*self.board_x*2)
+        self._board.append('-' + '-' * self.board_x * 2)
 
         for i in xrange(self.board_y):
-            self._board.append('|' + 'x'*self.board_x)
+            self._board.append('|' + self.cover * self.board_x)
 
-        self._board.append('-' + '-'*self.board_x*2)
+        self._board.append('-' + '-' * self.board_x * 2)
         self.board = self._board[:]
         self.start()
         self.print_board()
@@ -181,24 +198,24 @@ class Minesweeper(object):
         self.nums()
 
     def nums(self):
-        for i in xrange(1, self.board_y+1):
-            for j in xrange(1, self.board_x+1):
+        for i in xrange(1, self.board_y + 1):
+            for j in xrange(1, self.board_x + 1):
                 m = 0
-                if [j-1, i] in self.mines:
+                if [j - 1, i] in self.mines:
                     m += 1
-                if [j-1, i-1] in self.mines:
+                if [j - 1, i - 1] in self.mines:
                     m += 1
-                if [j, i-1] in self.mines:
+                if [j, i - 1] in self.mines:
                     m += 1
-                if [j+1, i-1] in self.mines:
+                if [j + 1, i - 1] in self.mines:
                     m += 1
-                if [j+1, i] in self.mines:
+                if [j + 1, i] in self.mines:
                     m += 1
-                if [j+1, i+1] in self.mines:
+                if [j + 1, i + 1] in self.mines:
                     m += 1
-                if [j, i+1] in self.mines:
+                if [j, i + 1] in self.mines:
                     m += 1
-                if [j-1, i+1] in self.mines:
+                if [j - 1, i + 1] in self.mines:
                     m += 1
                 self.numbers.append([j, i, m])  # oh, hi Jim!
 
@@ -208,17 +225,20 @@ class Minesweeper(object):
         self.numbers = _numbers[:]
 
     def print_board(self, x=None, y=None, end=False, found=False):
-        print(' '*self.padding + 'v Y / X ->')
+        # cache
+        pad = self.pad
+        spa = self.spa
+
+        print(pad + 'v Y / X ->')
         if not x and not y:
             for _row in self.board:
                 row = []
                 for r in _row:
                     if r != '|' and r != '-':
-                        row.append(r + ' '*self.spacing +
-                                   '|' + ' '*self.spacing)
+                        row.append(r + spa + '|' + spa)
                     else:
-                        row.append(r + ' '*self.spacing)
-                print(' '*self.padding + ''.join(row))
+                        row.append(r + spa)
+                print(pad + ''.join(row))
         else:
             if not end:
                 self.board[y] = list(self.board[y])
@@ -235,14 +255,14 @@ class Minesweeper(object):
                 row = []
                 for r in _row:
                     if r != '|' and r != '-':
-                        row.append(r + ' '*self.spacing +
-                                   '|' + ' '*self.spacing)
+                        row.append(r + spa + '|' + spa)
                     else:
-                        row.append(r + ' '*self.spacing)
-                print(' '*self.padding + ''.join(row).replace('0', self.empty))
+                        row.append(r + spa)
+
+                print(pad + ''.join(row).replace('0', self.empty))
             if end and not found:
-                print('Game Over')
-                if raw_input('Play again? y/n') != 'y':
+                print(self.pad + 'Game Over')
+                if raw_input(self.pad + 'Play again? y/n') != 'y':
                     exit()
                 else:
                     self.reset()
@@ -252,23 +272,25 @@ class Minesweeper(object):
         x, y = None, None
         found = False
         while not x and not y:
-            x = raw_input(' '*self.padding + 'x >>> ')
-            y = raw_input(' '*self.padding + 'y >>> ')
+            try:
+                x = raw_input(self.pad + 'x >>> ')
+                y = raw_input(self.pad + 'y >>> ')
+            except EOFError:
+                continue
 
             try:
                 x = int(x)
                 y = int(y)
-
             except ValueError:
+
                 try:
-                    if x[-1:] == self.found_mine and y[-1:] == self.found_mine:
+                    if x[-1:] == y[-1:] == self.found_mine:
                         x = int(x[:-1])
                         y = int(y[:-1])
                         found = True
                     else:
                         # if input is e.g. 1a, 2
                         x, y = 0, 0
-
                 except TypeError:
                     x, y = 0, 0
 
@@ -277,7 +299,7 @@ class Minesweeper(object):
                 x, y = 0, 0
 
         if x <= 0 or y <= 0:
-            print('Only positive numbers!')
+            print(self.pad + 'Only positive numbers!')
             return
 
         self.clean()
@@ -286,6 +308,7 @@ class Minesweeper(object):
 
     def reveal(self, x, y, found):
         zeros = [[x, y]]
+        num = self.numbers
 
         # where to look
         # corners
@@ -315,7 +338,7 @@ class Minesweeper(object):
             zeros.extend([[x + 1, y], [x + 1, y - 1], [x, y - 1],
                           [x - 1, y], [x - 1, y - 1]])
 
-        # core
+        # middle
         elif x > 1 and x < self.board_x and y > 1 and y < self.board_y:
             zeros.extend([[x + 1, y + 1], [x, y + 1], [x + 1, y],
                           [x + 1, y - 1], [x, y - 1], [x - 1, y],
@@ -323,31 +346,42 @@ class Minesweeper(object):
 
         for z in zeros:
             try:
-                if (self.numbers[z[1] - 1][z[0] - 1][2] == 0 and
-                        self.numbers[z[1] - 1][z[0] - 1] not in self.mines):
+                if (num[z[1] - 1][z[0] - 1][2] == 0 and
+                        num[z[1] - 1][z[0] - 1] not in self.mines):
                     self.clean()
-                    self.print_board(z[0], z[1],
-                                     [z[0], z[1]] in self.mines, found)
+                    self.print_board(
+                        z[0], z[1],
+                        [z[0], z[1]] in self.mines,
+                        found
+                    )
                 else:
                     self.clean()
-                    self.print_board(x, y, [x, y] in self.mines, found)
+                    self.print_board(
+                        x, y,
+                        [x, y] in self.mines,
+                        found
+                    )
                     return
 
             except IndexError:
                 self.print_board()
-                print('Out of bounds!')
+                print(self.pad + 'Out of bounds!')
 
     def check_end(self):
         revealed = 0
+        remaining = 0
         for row in self.board:
+            remaining += row.count(self.cover)
             revealed += row.count(self.found_mine)
-        if len(self.mines) == revealed:
-            print('Congrats! Game Over!')
+
+        if len(self.mines) in (revealed, remaining):
+            print(self.pad + 'Congrats!')
             raw_input()
             self.reset()
 
     def reset(self):
         self.__init__()
+
 
 if __name__ == "__main__":
     Minesweeper()
